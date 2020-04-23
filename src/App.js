37e9b93be8e8c6a071230b83d80063cf6b9f6c6e
MxCabrera <53961785+MxCabrera@users.noreply.github.com>
2020-04-23 17:22:08 -0400

    made my first component test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Main from './Main'
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {}
+  }
   render(){
     return (
       <header>
         <h1>Miguel Cabrera - React Project 5</h1>
+          <Main />
       </header>
     );
   }
---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Main extends Component {
+  render() {
+    return (
+      <main>
+          <h2>Bet ya didnt think id be on your page</h2>
+      </main>
+    )
+  }
+}
+
+export default Main;
