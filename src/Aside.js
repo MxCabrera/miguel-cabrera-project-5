3022b788271a06b33237d720e7a4329f13e4a5de
MxCabrera <53961785+MxCabrera@users.noreply.github.com>
2020-04-28 00:10:23 -0400

    added new fonts, and created nav in bottom of app

--- src/Aside.js
+++ src/Aside.js
@@ -3,16 +3,16 @@ import './App.css';
 
 function Aside() {
    return (
-      <aside>
-         {/* <div class="nav">
+      <aside className="wrapper">
+         <div class="nav">
             <nav>
                   <ul class="flex">
-                     <li><a href="">Breaking Bad API</a></li>
-                     <li><a href="">My Portfolio</a></li>
-                     <li><a href="">Official Website</a></li>
+                     <li><a href="https://breakingbadapi.com/documentation">Breaking Bad API</a></li>
+                     <li><a href="http://www.mcabrera.ca">My Portfolio</a></li>
+                  <li><a href="https://breakingbad.fandom.com/wiki/Breaking_Bad_Wiki">Official Website</a></li>
                   </ul>
             </nav>
-            </div> */}
+            </div>
          <h3 className="credits">Breaking Bad API was used to create this App. Photos and audio files are not mine, and are owned by there respective creators</h3>
       </aside>
    )
